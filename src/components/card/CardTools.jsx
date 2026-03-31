142d0348187e580d26fe4d570cf526492456d8b1
Puskor Roy <puskarkumarroy329@gmail.com>
2026-03-31 18:11:36 +0600

    card delete

--- src/components/card/CardTools.jsx
+++ src/components/card/CardTools.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import ShowEmpty from './ShowEmpty';
 
-const CardTools = ({cardStor,setCardStor}) => {
+const CardTools = ({ cardStor, setCardStor }) => {
+    const handelClickEmpty = () => {
+        setCardStor([])
+    }
     return (
         <div className='h-[400px] border'>
-            {cardStor.length >0 ? <div>{cardStor.map((item,index)=> <ShowCard key={index}  item={item} setCardStor={setCardStor}/>) }</div> : <ShowEmpty/>
+            {cardStor.length > 0 ? 
+            <div>
+                {cardStor.map((item, index) => <ShowCard key={index} item={item} setCardStor={setCardStor} />)}
+                <button onClick={() => handelClickEmpty()} className='btn btn-primary w-full mt-20 py-8'> All Clear</button>
+            </div>
+                : <ShowEmpty />
             }
         </div>
     );
--- src/components/card/ShowCard.jsx
+++ src/components/card/ShowCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const ShowCard = ({item,setCardStor}) => {
+    console.log(item);
+
+    // const handelFilter = ()=>{
+    //     setCardStor()
+    // }
+ 
     return (
-        <div>
-            <h1>
-                {item.name}
-            </h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 justify-between border border-gray-300 items-center'>
+            <div className='flex items-center text-center justify-center'>
+                <h1 className='py-4 w-full bg-red-400'>{item.name}</h1>
+                <h1 className='py-4 bg-green-200 w-full'>{item.price}</h1>
+            </div>
+            <button className='btn px-7 btn-primary h-full'>DELETE</button>
         </div>
     );
 };
